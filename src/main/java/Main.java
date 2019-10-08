8c761d5d974be7e560d9a3a426b37cacaa6e0034
Michał <uscinskimichal@gmail.com>
2019-10-08 15:10:29 +0200

    initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,6 +12,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        primaryStage.setResizable(false);
     }
 
 
