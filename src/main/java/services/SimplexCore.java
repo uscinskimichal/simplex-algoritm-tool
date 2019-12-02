d995653b27d838a73ba1e5df81dddd7a9e5ae2ef
Michał <uscinskimichal@gmail.com>
2019-12-02 13:57:47 +0100

    initial steps of simplex algorithm has been implemented

--- src/main/java/services/SimplexCore.java
+++ src/main/java/services/SimplexCore.java
@@ -1,45 +1,63 @@
 package services;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import util.Configuration;
 
 public class SimplexCore {
 
-    List<BigDecimal> variables;
-    List<List<BigDecimal>> constraints;
-    List<String> constraintsMark;
-    List<BigDecimal> constraintsRightSide = new ArrayList<>();
+    private final int numberOfConstraints;
+    List<BigDecimal> listOfVariables;
+    List<List<BigDecimal>> listOfConstraints;
+    List<String> listOfConstraintsMark;
+    List<BigDecimal> listRightSideOfConstraints = new ArrayList<>();
+    List<Integer> listOfBasisIndexes = new ArrayList<>();
+    List<BigDecimal> listOfCoefficientsOfVariables;
+    List<BigDecimal> listOfDifferenceVariablesAndCoefficients;
+    List<BigDecimal> listOfBasicConstraintsDividedByXi;
     boolean maximization;
 
-    public SimplexCore(List<BigDecimal> variables, List<List<BigDecimal>> constraints, boolean maximization, List<String> constraintsMark) {
-        this.variables = variables;
-        this.constraints = constraints;
+    public SimplexCore(List<BigDecimal> listOfVariables, List<List<BigDecimal>> listOfConstraints, boolean maximization, List<String> listOfConstraintsMark) {
+        this.listOfVariables = listOfVariables;
+        this.listOfConstraints = listOfConstraints;
         this.maximization = maximization;
-        this.constraintsMark = constraintsMark;
-        parseRightSidesOfConstraints();
+        this.listOfConstraintsMark = listOfConstraintsMark;
+        this.numberOfConstraints = listOfConstraints.size();
+        extractRightSidesOfConstraints();
+        normalize();
+
+        listOfBasicConstraintsDividedByXi = new ArrayList<BigDecimal>(Collections.nCopies(numberOfConstraints, new BigDecimal("0.0")));
+        listOfCoefficientsOfVariables = new ArrayList<BigDecimal>(Collections.nCopies(this.listOfVariables.size(), new BigDecimal("0.0")));
+        listOfDifferenceVariablesAndCoefficients = new ArrayList<BigDecimal>(Collections.nCopies(this.listOfVariables.size(), new BigDecimal("0.0")));
+
         //writeData();
-        normalizeRestrictions();
-        writeData();
+        printBasisIndexes();
+
+        calculateCoeficcients();
+        calculateVariablesAndCoeffDifference();
+
+        calculateDecisionVector();
     }
 
-    private void parseRightSidesOfConstraints() {
-        for (int i = 0; i < constraints.size(); i++) {
-            int element = constraints.get(i).size() - 1;
-            constraintsRightSide.add(constraints.get(i).get(element));
-            constraints.get(i).remove(element);
-            System.out.println("PRAWA STRONA OGRANICZENIA :" + i + " = " + constraintsRightSide.get(i));
+    private void extractRightSidesOfConstraints() {
+        for (int i = 0; i < listOfConstraints.size(); i++) {
+            int element = listOfConstraints.get(i).size() - 1;
+            listRightSideOfConstraints.add(listOfConstraints.get(i).get(element));
+            listOfConstraints.get(i).remove(element);
+            System.out.println("PRAWA STRONA OGRANICZENIA :" + i + " = " + listRightSideOfConstraints.get(i));
         }
     }
 
 
     private void writeData() {
         System.out.println("F : CELU:");
-        variables.forEach(a -> System.out.println(a.doubleValue()));
+        listOfVariables.forEach(a -> System.out.println(a.doubleValue()));
         System.out.println("OGARNICZENIA");
-        constraints.forEach(a -> {
+        listOfConstraints.forEach(a -> {
             System.out.println("---------");
             a.forEach(
                     b -> System.out.println(b.doubleValue()));
@@ -47,48 +65,111 @@ public class SimplexCore {
         System.out.println("KREYTERIUM");
         System.out.println(maximization);
         System.out.println("ZNAKI OGRANICZEN");
-        constraintsMark.forEach(a -> System.out.println(a));
+        listOfConstraintsMark.forEach(a -> System.out.println(a));
     }
 
     /// TO DO jezeli MAX to -MAi jezeli MIN to +MAi
-    private void normalizeRestrictions() {
-        for (int i = 0; i < constraintsMark.size(); i++) {
-            if (constraintsMark.get(i).equals(">=")) {
-                for (int j = 0; j < constraints.size(); j++) {
+    private void normalize() {
+        for (int i = 0; i < listOfConstraintsMark.size(); i++) {
+            if (listOfConstraintsMark.get(i).equals(">=")) {
+                for (int j = 0; j < listOfConstraints.size(); j++) {
                     if (i == j) {
-                        constraints.get(j).add(new BigDecimal("-1.0")); // sub Si
-                        constraints.get(j).add(new BigDecimal("1.0")); // add Ai
-                        variables.add(new BigDecimal("0.0"));
+                        listOfConstraints.get(j).add(new BigDecimal("-1.0")); // sub Si
+                        listOfConstraints.get(j).add(new BigDecimal("1.0")); // add Ai
+                        listOfBasisIndexes.add(listOfConstraints.get(j).size() - 1);
+                        listOfVariables.add(new BigDecimal("0.0"));
                         if (maximization)
-                            variables.add(new BigDecimal("-" + Configuration.M));
+                            listOfVariables.add(new BigDecimal("-" + Configuration.M));
                         else
-                            variables.add(new BigDecimal("" + Configuration.M));
+                            listOfVariables.add(new BigDecimal("" + Configuration.M));
 
                     } else {
-                        constraints.get(j).add(new BigDecimal("0.0"));
-                        constraints.get(j).add(new BigDecimal("0.0"));
+                        listOfConstraints.get(j).add(new BigDecimal("0.0"));
+                        listOfConstraints.get(j).add(new BigDecimal("0.0"));
                     }
                 }
-            } else if (constraintsMark.get(i).equals("<=")) {
-                for (int j = 0; j < constraints.size(); j++) {
+            } else if (listOfConstraintsMark.get(i).equals("<=")) {
+                for (int j = 0; j < listOfConstraints.size(); j++) {
                     if (i == j) {
-                        constraints.get(j).add(new BigDecimal("1.0"));
-                        variables.add(new BigDecimal("0.0"));
+                        listOfConstraints.get(j).add(new BigDecimal("1.0"));
+                        listOfBasisIndexes.add(listOfConstraints.get(j).size() - 1);
+                        listOfVariables.add(new BigDecimal("0.0"));
                     } else
-                        constraints.get(j).add(new BigDecimal("0.0"));
+                        listOfConstraints.get(j).add(new BigDecimal("0.0"));
                 }
             } else {
-                for (int j = 0; j < constraints.size(); j++) {
+                for (int j = 0; j < listOfConstraints.size(); j++) {
                     if (i == j) {
-                        constraints.get(j).add(new BigDecimal("1.0"));
+                        listOfConstraints.get(j).add(new BigDecimal("1.0"));
+                        listOfBasisIndexes.add(listOfConstraints.get(j).size() - 1);
                         if (maximization)
-                            variables.add(new BigDecimal("-" + Configuration.M));
+                            listOfVariables.add(new BigDecimal("-" + Configuration.M));
                         else
-                            variables.add(new BigDecimal("" + Configuration.M));
+                            listOfVariables.add(new BigDecimal("" + Configuration.M));
                     } else
-                        constraints.get(j).add(new BigDecimal("0.0"));
+                        listOfConstraints.get(j).add(new BigDecimal("0.0"));
                 }
             }
         }
+    }
+
+    private void printBasisIndexes() {
+        System.out.println("INDEKSY BAZY:");
+        listOfBasisIndexes.forEach(a -> System.out.println(a.doubleValue()));
+    }
+
+    private void calculateCoeficcients() {
+        System.out.println("Cj:");
+
+        for (int i = 0; i < listOfVariables.size(); i++) {
+            for (int j = 0; j < numberOfConstraints; j++) {
+
+                listOfCoefficientsOfVariables.set(i,
+                        listOfCoefficientsOfVariables.get(i)
+                                .add(listOfConstraints.get(j).get(i)
+                                        .multiply(listOfVariables.get(listOfBasisIndexes.get(j)))));
+
+            }
+            System.out.println(i + " " + listOfCoefficientsOfVariables.get(i));
+        }
+    }
+
+    private void calculateVariablesAndCoeffDifference() {
+        System.out.println(" Zj - Cj");
+        for (int i = 0; i < listOfVariables.size(); i++) {
+            listOfDifferenceVariablesAndCoefficients.set(i, listOfVariables.get(i).subtract(listOfCoefficientsOfVariables.get(i)));
+
+            System.out.println(i + "  " + listOfDifferenceVariablesAndCoefficients.get(i));
+        }
+    }
+
+    private int getPivotElementMAX() {
+        System.out.println("MAX ELEMENT : " + Collections.max(listOfDifferenceVariablesAndCoefficients));
+        return listOfDifferenceVariablesAndCoefficients.indexOf(Collections.max(listOfDifferenceVariablesAndCoefficients));
+    }
+
+    private int getPivotElementMIN() {
+        System.out.println("MIN ELEMENT : " + Collections.min(listOfDifferenceVariablesAndCoefficients));
+        return listOfDifferenceVariablesAndCoefficients.indexOf(Collections.min(listOfDifferenceVariablesAndCoefficients));
+    }
+
+    private void calculateDecisionVector() {
+        int index;
+        if (maximization)
+            index = getPivotElementMAX();
+        else
+            index = getPivotElementMIN();
+
+        int i = 0;
+        try {
+            for (i = 0; i < numberOfConstraints; i++) {
+                listOfBasicConstraintsDividedByXi
+                        .set(i, listRightSideOfConstraints.get(i)
+                                .divide(listOfConstraints.get(i).get(index), 4, RoundingMode.HALF_UP));
+            }
+        } catch (ArithmeticException ae) {
+            listOfBasicConstraintsDividedByXi.set(i, Configuration.M);
+        }
+        listOfBasicConstraintsDividedByXi.forEach(a -> System.out.println("DECISION VECTOR : " + a));
     }
 }
